8e7fa7a7dcf391c92e6b4ae4749afba3f0c872de
Somers Matthews <somersbmatthews@gmail.com>
2019-12-05 07:52:42 -0600

    first commit

---
+++ android/app/src/main/java/com/instacook_react_native_vanilla/MainActivity.java
@@ -0,0 +1,15 @@
+package com.instacook_react_native_vanilla;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "instacook_react_native_vanilla";
+  }
+}
